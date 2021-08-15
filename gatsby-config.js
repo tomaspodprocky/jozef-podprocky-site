174634b7825df51645ebd94f1d431efee2bb810b
Tomas Podprocky <tomas.podprocky@gmail.com>
2021-08-15 22:02:32 +0200

    Initial commit from gatsby: (https://github.com/tomaspodprocky/gatzby-tutorial-site.git)

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "tutorialSite",
+  },
+  plugins: ["gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"],
+};

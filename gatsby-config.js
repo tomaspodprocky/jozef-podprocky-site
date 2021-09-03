8ca355e25d04b7cfcfa1b4a82c7acceaf4ab1abe
Tomas Podprocky <tomas.podprocky@gmail.com>
2021-09-03 21:35:01 +0200

    Continue - tutorial Part 4. To finish

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "tutorialSite",
+    title: "First tutorial site",
   },
   plugins: ["gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
